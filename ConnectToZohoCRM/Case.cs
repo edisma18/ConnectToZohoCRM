eac1256c937e8c7b73e283d3cb8babca3628475c
Ismael Lopez <Ismael Lopez@DESKTOP-KDQRJGE>
2024-10-09 14:48:50 -0600

    Add project files.

---
+++ ConnectToZohoCRM/Case.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+using System.Text.Json.Serialization;
+
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+public class Model
+{
+    public List<Case> items { get; set; }
+}
+
+public record class data()
+{
+    public Case caseCRM { get; set; }
+}
+public record class Deal_Name()
+{
+    [JsonProperty("Deal_Name")]
+    public string name { get; set; }
+}
+public record class Case()
+
+    {
+
+    [JsonProperty("data")]
+    public string data { get; set; }
+
+    [JsonProperty("id")]
+    public string id { get; set; }
+
+    [JsonProperty("Subject")]
+    public string Subject { get; set; }
+
+    [JsonProperty("Case_Number")]
+    public string Case_Number { get; set; }
+
+    [JsonProperty("Deal_Name")]
+
+    public Deal_Name Deal_Name { get; set; } 
+}
+
+public class Token
+{
+    public string access_token { get; set; }
+    public string scope { get; set; }
+}
+
+namespace WebAPIClient
+{
+    internal class Case
+    {
+    }
+
+}
---
+++ ConnectToZohoCRM/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
+
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+var repositories = await GetCasesAsync(client);
+
+foreach (var repo in repositories)
+{
+    Console.WriteLine($"id: {repo.id}");
+    Console.WriteLine($"Subject: {repo.Subject}");
+    Console.WriteLine($"Case_Number : {repo.Case_Number}");
+    Console.WriteLine($"Deal_Name : {repo.Deal_Name.name}");
+
+
+    Console.WriteLine();
+}
+
+static async Task<IList<Case>> GetCasesAsync(HttpClient client)
+{
+
+    var clientToken = new HttpClient();
+    var request = new HttpRequestMessage(HttpMethod.Post, "https://accounts.zoho.com/oauth/v2/token?refresh_token=1000.977114ec827f39efec9977206a8de550.0f4ad2e0e0b642a94d36eb1bb472de5a&client_id=1000.WDK9A9IX2M3E7VQV7G60TX00R9JSWB&client_secret=f1aa7083bc81106754ca6379020596b83e4f956279&grant_type=refresh_token");
+    request.Headers.Add("Cookie", "JSESSIONID=9C330F962792359E620D82159FAC5B2E; _zcsr_tmp=eceda221-658e-471b-b4a0-3eea21570e90; iamcsr=eceda221-658e-471b-b4a0-3eea21570e90; zalb_b266a5bf57=a7f15cb1555106de5ac96d088b72e7c8");
+    var responseToken = await clientToken.SendAsync(request);
+    responseToken.EnsureSuccessStatusCode();
+    string resultTokenString = await responseToken.Content.ReadAsStringAsync();
+
+    Console.WriteLine(resultTokenString);
+
+    Token? accessToken =
+               JsonSerializer.Deserialize<Token>(resultTokenString);
+
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+        "Bearer", accessToken.access_token);
+
+    client.DefaultRequestHeaders.Accept.Add(
+                                 new MediaTypeWithQualityHeaderValue("application/json"));
+
+    HttpResponseMessage response = client.GetAsync("https://www.zohoapis.com/crm/v7/Cases?fields=Account_Name,Deal_Name,Subject,Case_Number&per_page=2").Result;  
+    response.EnsureSuccessStatusCode();
+    string resultContentString = await response.Content.ReadAsStringAsync();
+
+    JObject casesSearch = JObject.Parse(resultContentString);
+    IList<JToken> results = casesSearch["data"].Children().ToList();
+    IList<Case> searchResults = new List<Case>();
+
+    foreach (JToken result in results)
+    {
+        Case searchResult = result.ToObject<Case>();
+        searchResults.Add(searchResult);
+    }
+
+
+    return searchResults;
+
+}
